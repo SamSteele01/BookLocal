5a3ed7577de7b1c82c4c0f6d52f055c03300713c
SamSteele01 <ssteele017@gmail.com>
2018-02-04 20:30:43 -0500

    Move header to component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          {/* contact dropdown */}
+        </header>
+      </div>
+    );
+  }
+
+}
+
+export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import Header from '../components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h2>Book Local</h2>
-        </header>
-        
+        <Header/>
+
       </div>
     );
   }
