fd6583821ecacd1b997c3f16a519a58fd3af7933
SamSteele01 <ssteele017@gmail.com>
2018-02-05 21:21:31 -0500

    files from $bower install grommet

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Jumbotron extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>(Front desk or relevent image, parallaxing)</h2>
+      </div>
+    );
+  }
+
+}
+
+export default Jumbotron;
---
+++ src/containers/what/What.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Paragraph from 'grommet/components/Paragraph';
+
+class What extends Component {
+
+  render() {
+    return (
+      <div>
+        <Paragraph>
+          Blah, blah blah
+        </Paragraph>
+      </div>
+    );
+  }
+
+}
+
+export default What;
