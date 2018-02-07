f1e7ac452c22c0488be94860a9da0321fe2afa79
SamSteele01 <ssteele017@gmail.com>
2018-02-07 13:15:00 -0500

    Create container

---
+++ src/containers/why/Solutions.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Solutions extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Solutions</h3>
+      </div>
+    );
+  }
+
+}
+
+export default Solutions;
