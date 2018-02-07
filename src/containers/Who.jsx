001e1fcd8125de43fbe914ecefd09e7e8f5b30ff
SamSteele01 <ssteele017@gmail.com>
2018-02-07 10:49:53 -0500

    Create 'Who' container.

---
+++ src/containers/Who.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Who extends Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+
+}
+
+export default Who;
