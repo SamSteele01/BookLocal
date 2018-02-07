cc8cb4499693125dcd7e41edcc8292406c198236
SamSteele01 <ssteele017@gmail.com>
2018-02-07 13:14:00 -0500

    Move to folder

--- src/containers/why/Why.jsx
+++ src/containers/why/Why.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Section from 'grommet/components/Section';
 import Label from 'grommet/components/Label';
+import Problems from './Problems';
+import CustomList from '../CustomList';
+import CustomListItem from '../CustomListItem';
 
 class Why extends Component {
 
@@ -14,6 +17,12 @@ class Why extends Component {
           To understand the motivation for BookLocal we identify a few key issues with
           the current hotel reservation model and propose solutions.
         </Label>
+        <Problems>
+          <CustomList title="High commission payments.">
+            <CustomListItem text="Online travel agents (OTA) receive 15-25% commission per room."/>
+          </CustomList>
+        </Problems>
+        {/* <Solutions/> */}
       </div>
     );
   }
