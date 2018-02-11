2cec105f3f502c27c15c71081436a688d169c434
SamSteele01 <ssteele017@gmail.com>
2018-02-11 16:43:30 -0500

    Comment out srcSet

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -10,7 +10,8 @@ class Logo extends Component {
           className="Logo" src={Logo5} 
           alt=""
           width="320"
-          height="132" srcSet="http://booklocal.in/wp-content/uploads/2017/07/logo5.png 320w, http://booklocal.in/wp-content/uploads/2017/07/logo5-768x316.png 768w"
+          height="132" 
+          // srcSet="http://booklocal.in/wp-content/uploads/2017/07/logo5.png 320w, http://booklocal.in/wp-content/uploads/2017/07/logo5-768x316.png 768w"
           sizes="(max-width: 320px) 100vw, 320px"
         />
       </div>
