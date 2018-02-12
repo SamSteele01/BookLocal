fd875e278817d240fdfc7bdffd2807ad9b60b9b9
SamSteele01 <ssteele017@gmail.com>
2018-02-11 22:41:32 -0500

    Create components

---
+++ src/components/Icon.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+const {PropTypes} = React;
+
+const Icon = props => {
+  const styles = {
+    svg: {
+      display: 'inline-block',
+      verticalAlign: 'middle',
+    },
+    path: {
+      fill: props.color,
+    },
+  };
+
+//   change to <img>
+  return (
+    <svg
+      style={styles.svg}
+      width={`${props.size}px`}
+      height={`${props.size}px`}
+      viewBox="0 0 1024 1024"
+    >
+      <path
+        style={styles.path}
+        d={props.icon}
+      ></path>
+    </svg>
+  );
+};
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  size: 16,
+};
+
+export default Icon;
