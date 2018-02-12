af8e68642104b87234935b4e3520d3f524eca448
SamSteele01 <ssteele017@gmail.com>
2018-02-12 17:45:34 -0500

    Add props and propTypes.

--- src/containers/CustomListItem.jsx
+++ src/containers/CustomListItem.jsx
@@ -1,34 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Animate from 'grommet/components/Animate';
 import Box from 'grommet/components/Box';
 
 class CustomListItem extends Component {
 
   render() {
+
+    const {
+      height,
+      animation,
+      color,
+      bgColor,
+      border,
+      borderRad,
+      classNames
+    } = this.props;
+
+    var divStyle = {
+      color: color, 
+      backgroundColor: bgColor,
+      border: border,
+      borderRadius: borderRad
+    };
+
     return (
       <div>
-        <Animate enter={{"animation": "slide-right", "duration": 1000, "delay": 0}}
+        <Animate enter={{"animation": animation, "duration": 1000, "delay": 0}}
             keep={true} visible="scroll">
-          <Box
-            direction='row'
-            justify='between'
-            alignSelf='stretch'
-            // full='horizontal'
-            flex='grow'
-            // size='medium'
-            responsive={false}
-            wrap={false}
-            pad='small'
-            margin='small'
-            colorIndex={this.props.colorIndex}>
-            {this.props.children}
-          </Box>
+          <div style={divStyle} className={classNames}>
+            <Box
+              direction='row'
+              justify='between'
+              align='center'
+              alignSelf='stretch'
+              // full='horizontal'
+              flex='grow'
+              size={{height: height, width: 'full'}}
+              responsive={false}
+              wrap={false}
+              pad='large'
+              margin='small'
+              >
+              {this.props.children}
+            </Box>
+          </div>
         </Animate>
       </div>
     );
   }
 }
 
+CustomListItem.propTypes = {
+  // children: PropTypes.element,
+  // icon: PropTypes.string.isRequired,
+  height: PropTypes.string,
+  animation: PropTypes.string,
+  color: PropTypes.string,
+  bgColor: PropTypes.string,
+  classNames: PropTypes.string
+};
+
+CustomListItem.defaultProps = {
+  height: "xsmall",
+  animation: "slide-right",
+  color: "#464749", //font-gray
+  bgColor: "white"
+};
 
 
 export default CustomListItem;
