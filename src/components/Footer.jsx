ab610e17c4aa67588e30df06a23ba714bf601159
SamSteele01 <ssteele017@gmail.com>
2018-02-15 18:25:48 -0500

    Create component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="App-footer">
+                    Copyright © 2018 BookLocal, All rights reserved
+                </footer>
+            </div>
+        )
+    }
+}
