20ca2957e6b9cbd9a08aacf7cd151ed8756b5acd
Hannah Hudson <hhudson@riversagency.com>
2018-02-17 12:46:44 -0500

    styling and structural fixes to header and logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Logo5 from '../images/logo5.png';
 
 class Header extends Component {
 
@@ -7,6 +8,12 @@ class Header extends Component {
       <div>
         <header className="App-header">
           {/* contact dropdown */}
+          <img
+            src={Logo5}
+            alt="Book Local"
+            class="logo"
+            // srcSet="http://booklocal.in/wp-content/uploads/2017/07/logo5.png 320w, http://booklocal.in/wp-content/uploads/2017/07/logo5-768x316.png 768w"
+          />
         </header>
       </div>
     );
