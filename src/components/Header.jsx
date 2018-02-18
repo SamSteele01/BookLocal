ea28ddf879f49b795da539d6e5fb6edbadc0a781
SamSteele01 <ssteele017@gmail.com>
2018-02-17 22:51:27 -0500

    Remove comment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,10 @@ class Header extends Component {
     return (
       <div>
         <header className="App-header">
-          {/* add links to navigate */}
           <nav className="navbar pure-menu pure-menu-horizontal">
-          <Link to="/home" className="pure-menu-heading pure-menu-link">Home</Link>
-          <Link to="/about" className="pure-menu-heading pure-menu-link">About</Link>
-          <Link to="/register" className="pure-menu-heading pure-menu-link">Register</Link>
-        
+            <Link to="/home" className="pure-menu-heading pure-menu-link">Home</Link>
+            <Link to="/about" className="pure-menu-heading pure-menu-link">About</Link>
+            <Link to="/register" className="pure-menu-heading pure-menu-link">Register</Link>
           </nav>
         </header>
       </div>
