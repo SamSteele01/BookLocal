d2dbe27f90e87504821471fbf7c88a5288de0a4c
Hannah Hudson <hhudson@riversagency.com>
2018-02-24 17:52:30 -0500

    early adopters section

--- src/containers/Steps.jsx
+++ src/containers/Steps.jsx
@@ -6,6 +6,9 @@ import CustomListItem from './CustomListItem';
 import colors from '../styles/colors';
 import Helpers from "../styles/Helpers.scss";
 import WalletImg from "../images/wallet.jpg";
+import CouchImg from "../images/couch.jpg";
+import HotelImg from "../images/hotel.jpg";
+import TravelersImg from "../images/travelers.jpg";
 
 export default class Steps extends Component {
     render() {
@@ -288,54 +291,60 @@ export default class Steps extends Component {
 
 
                 {/* 5.2 */}
-                <Heading align='center'
- 	 	 	 	 	 	 margin='medium'
-                    tag='h2'>
+                <Heading align='center' margin='medium' tag='h2' className="heading">
                     Early Adopters
                 </Heading>
-                <p className='paragraph-white'>
-                    Once the BookLocal platform is successfully tested at the Exchange Building, we
+                <h3>Once the BookLocal platform is successfully tested at the Exchange Building, we
                     will market the application for use by adventurous travelers, independent hotels,
-                    and individuals with extra space. Toward this end, our goal is to provide the
-                    following:
-                </p>
+                    and individuals with extra space. Once we have a strong community of users, we will target chain hotels by adding
+                        features that allow for corporate oversight on overall and hotel specific performance. Toward this end, our goal is to provide the
+                    following:</h3>
+
 
                 {/* 5.2.1 */}
-                <Heading align='center'
- 	 	 	 	 	 	 margin='medium'
-                    tag='h3'>
+                <div className="content-card--wrapper">
+                  <div className="content-card">
+                <Heading align='center' margin='medium' tag='h3'>
                     Free interface for travelers.
                 </Heading>
-                <p className='paragraph-white'>
+                <p className='paragraph'>
                     In particular, we will target the Ethereum development community in order to
                     nurture early versions of the product.
                 </p>
+                <img className="image-md" alt="hotel-bed" src={TravelersImg}/>
+
+              </div>
 
                 {/* 5.2.2 */}
-                <Heading align='center'
- 	 	 	 	 	 	 margin='medium'
-                    tag='h3'>
+                <div className="content-card">
+                <Heading align='center' margin='medium' tag='h3'>
                     Free interface for individual hosts.
                 </Heading>
-                <p className='paragraph-white'>
+                <p className='paragraph'>
                     In order to encourage use, we will provide a free interface for individuals to rent
                     access to their extra space (similar to the AirBnB model).
                 </p>
+                <img className="image-md" alt="hotel-bed" src={CouchImg}/>
+
+              </div>
 
                 {/* 5.2.3 */}
-                <Heading align='center'
- 	 	 	 	 	 	 margin='medium'
-                    tag='h3'>
+                <div className="content-card">
+                <Heading align='center' margin='medium' tag='h3'>
                     Paid interface for hotels.
                 </Heading>
-                <p className='paragraph-white'>
+                <p className='paragraph'>
                     To address unique attributes of each hotel, we plan to offer custom hotel interfaces
                     as a service (i.e. software-as-a-service) in order to provide each early
                     adopting hotel with their ideal property management system.
                 </p>
+                <img className="image-md" alt="hotel-bed" src={HotelImg}/>
+
+              </div>
+            </div>
 
                 {/* 5.3 */}
-                <Heading align='center'
+                {/*<Heading align='center'
  	 	 	 	 	 	 margin='medium'
                     tag='h2'>
                     More Adopters
@@ -343,7 +352,7 @@ export default class Steps extends Component {
                 <p className='paragraph-white'>
                     Once we have a strong community of users, we will target chain hotels by adding
                     features that allow for corporate oversight on overall and hotel specific performance.
-                </p>
+                </p> */}
                 <div className="textbox">
                   <h2>Contracts</h2>
                   <p>The contracts for the proof of concept (written in solidity) are available online.</p>
