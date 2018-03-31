460659a31a13a60ca1815cb8e65c9401d2daaafe
SamSteele01 <ssteele017@gmail.com>
2018-03-30 21:29:56 -0400

    Add/change classNames

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div className="App-footer">
+            <div className="footer">
                 <footer >
                     Copyright © 2018 BookLocal, All rights reserved
                 </footer>
