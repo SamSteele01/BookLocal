0561f9018a748b5b841770f079bd402319d96e69
SamSteele01 <ssteele017@gmail.com>
2018-03-31 00:16:19 -0400

    Comment out unused alias

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -91,7 +91,7 @@ module.exports = {
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
       react: path.resolve('./node_modules/react'),
-      'bl-comp': path.resolve(__dirname, '../src/components')
+      // 'bl-comp': path.resolve(__dirname, 'src/components')
     },
     // plugins: [
     //   // Prevents users from importing files from outside of src/ (or node_modules/).
