caae7c50ec8f1a4e58324042419cda9a187592dc
SamSteele01 <ssteele017@gmail.com>
2018-04-05 23:40:35 -0400

    Add action types for 'data'

--- src/const/constants.js
+++ src/const/constants.js
@@ -5,3 +5,22 @@ export const GO_BACK = 'ROUTER/GO_BACK';
 export const GO_FORWARD = 'ROUTER/GO_FORWARD';
 export const LOCATION_CHANGE = 'ROUTER/LOCATION_CHANGE';
 
+export const WEB3 = 'DATA/WEB3';
+export const WEB3_ERROR = 'DATA/WEB3_ERROR';
+export const CURRENT_NETWORK = 'DATA/CURRENT_NETWORK';
+export const RR = 'DATA/RR';
+
+export const TOKEN_ID = 'DATA/TOKEN_ID';
+export const ETH_MEM_START = 'DATA/ETH_MEM_START';
+export const ETH_MEM_STOP = 'DATA/ETH_MEM_STOP';
+export const RESPONSE_TXN = 'DATA/RESPONSE_TXN';
+export const CHECK_IN_DATE = 'DATA/CHECK_IN_DATE';
+export const CHECK_OUT_DATE = 'DATA/CHECK_OUT_DATE';
+export const RENTER_ADDRESS_ARRAY = 'DATA/RENTER_ADDRESS_ARRAY';
+export const OWNER = 'DATA/OWNER';
+export const RENTER = 'DATA/RENTER';
+export const MIN_RENT_TIME = 'DATA/MIN_RENT_TIME';
+export const ROOM_NUMBER = 'DATA/ROOM_NUMBER';
+export const NUM_BEDS = 'DATA/NUM_BEDS';
+
+export const DATA_CHANGE = 'DATA/DATA_CHANGE';
