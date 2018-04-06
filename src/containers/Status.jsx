7e097d6c8d39ec2daa43ee9dd6f744b553d81a50
SamSteele01 <ssteele017@gmail.com>
2018-04-06 17:11:54 -0400

    Pass props to components. Add propTypes

--- src/containers/Status.jsx
+++ src/containers/Status.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import GetNextReservation from 'components/GetNextReservation';
 import GetRoomInfo from 'components/GetRoomInfo';
 import GetReservations from 'components/GetReservations';
@@ -7,10 +8,18 @@ export default class Status extends Component {
   render() {
     return (
         <div className="app">
-            <GetNextReservation />
-            <GetRoomInfo />
-            <GetReservations />
+            <GetNextReservation web3={this.state.web3} RR={this.state.RR} returnComponentState={this.returnComponentState}/>
+            <GetRoomInfo web3={this.state.web3} RR={this.state.RR} tokenId={this.state.tokenId}/>
+            <GetReservations web3={this.state.web3} RR={this.state.RR} tokenId={this.state.tokenId} checkInDate={this.state.checkInDate} checkOutDate={this.state.checkOutDate} />
         </div>
     )
   }
+}
+Status.propTypes = {
+    web3: PropTypes.object,
+    RR: PropTypes.object,
+    tokenId: PropTypes.string, //?
+    returnComponentState: PropTypes.func,
+    checkInDate: PropTypes.object, //?
+    checkOutDate: PropTypes.object
 }
