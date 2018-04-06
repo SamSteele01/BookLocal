a2167966e5f31d2dcb307e3a7d60ebd4119e3b0e
SamSteele01 <ssteele017@gmail.com>
2018-04-05 23:37:15 -0400

    Create 'actions' folder

---
+++ src/actions/dataActions.js
@@ -0,0 +1,47 @@
+import { WEB3, WEB3_ERROR, CURRENT_NETWORK, RR, ETH_MEM_START, ETH_MEM_STOP, RESPONSE_TXN, CHECK_IN_DATE, CHECK_OUT_DATE } from 'const/constants';
+
+export const web3 = (instance) => ({
+  type: WEB3,
+  payload: instance,
+});
+export const web3error = (error) => ({
+  type: WEB3_ERROR,
+  payload: error,
+});
+export const currentNetwork = (index) => ({
+  type: CURRENT_NETWORK,
+  payload: index,
+});
+export const rr = (contract) => ({
+  type: RR,
+  payload: contract,
+});
+export const ethMemStart = (date) => ({
+  type: ETH_MEM_START,
+  payload: date,
+});
+export const ethMemStop = (date) => ({
+  type: ETH_MEM_STOP,
+  payload: date,
+});
+export const responseTxn = (txn) => ({
+  type: RESPONSE_TXN,
+  payload: txn,
+});
+export const checkInDate = (date) => ({
+  type: CHECK_IN_DATE,
+  payload: date,
+});
+export const checkOutDate = (date) => ({
+  type: CHECK_OUT_DATE,
+  payload: date,
+});
+// export const locationChange = ({ pathname, search, hash }) => ({
+//   type: LOCATION_CHANGE,
+//   payload: {
+//     pathname,
+//     search,
+//     // queries: parse(search),
+//     hash,
+//   },
+// });
