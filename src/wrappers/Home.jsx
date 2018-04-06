4e923854d42ef136a402b84fbb4789edcd985701
SamSteele01 <ssteele017@gmail.com>
2018-04-06 15:45:06 -0400

    Move to wrappers folder

--- src/wrappers/Home.jsx
+++ src/wrappers/Home.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import RegisterMessage from './RegisterMessage';
 
 export default class Home extends Component {
     render() {
         return (
             <div className='home'>
-                {/* <h1>Home</h1> */}
-                <RegisterMessage/>
+                {this.props.children}
             </div>
         )
     }
