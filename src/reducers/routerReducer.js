70552a8666ca8dbb30d1c4bbfedb96e2d698149c
SamSteele01 <ssteele017@gmail.com>
2018-04-05 23:42:23 -0400

    Comment out unneeded code

--- src/reducers/routerReducer.js
+++ src/reducers/routerReducer.js
@@ -1,4 +1,5 @@
-import { LOCATION_CHANGE } from 'const/constants';
+// import { push, replace, go, goBack, goForward, locationChange } from 'actions';
+import { PUSH, REPLACE, GO, GO_BACK, GO_FORWARD, LOCATION_CHANGE } from 'const/constants';
 
 const initialState = {
   pathname: '/home',
@@ -9,11 +10,34 @@ const initialState = {
 
 export const routerReducer = (state = initialState, action) => {
   switch (action.type) {
+    // case PUSH:
+    //   return Object.assign({}, state, {
+    //     pathname: action.payload
+    //   })
+    // case REPLACE:
+    //   return {
+    //     ...state,
+    //     ...action.payload,
+    //   }
+    // case GO:
+    //   return Object.assign({}, state, {
+    //       visibilityFilter: action.filter
+    //     })
+    // case GO_BACK:
+    //   return {
+    //     ...state,
+    //     ...action.payload,
+    //   }
+    // case GO_FORWARD:
+    //   return {
+    //     ...state,
+    //     ...action.payload,
+    //   } // not needed here, in middleware
     case LOCATION_CHANGE:
       return {
         ...state,
         ...action.payload,
-      };
+      }
     default:
       return state;
   }
