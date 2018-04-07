5f811e874232ef0d360080e5be4d74cdc07d03fe
SamSteele01 <ssteele017@gmail.com>
2018-04-07 01:17:28 -0400

    Change text

--- src/components/RegisterMessage.jsx
+++ src/components/RegisterMessage.jsx
@@ -6,7 +6,8 @@ export default class RegisterMessage extends Component {
             <div className="home">
                 <div className="register-message">
                     <h2>
-                        Register for a free stay at the Exchange Building during ETHMemphis this May. <br/>Sponsored by BookLocal.
+                        {/* Register for a free stay at the Exchange Building during ETHMemphis this May. <br/>Sponsored by BookLocal. */}
+                        Welcome to the book local booking app. If you've received an email about a free stay at ethmemphis click here, to learn more about us click here.
                     </h2>
                 </div>
             </div>
