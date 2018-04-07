8cd888760591dc5dc35391ff34456baf96ee8aba
SamSteele01 <ssteele017@gmail.com>
2018-04-07 15:55:39 -0400

    Add links to Register Message

--- src/components/RegisterMessage.jsx
+++ src/components/RegisterMessage.jsx
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from "react-router-dom"
 
-export default class RegisterMessage extends Component {
-    render() {
-        return (
-            <div className="home">
-                <div className="register-message">
-                    <h2>
-                        {/* Register for a free stay at the Exchange Building during ETHMemphis this May. <br/>Sponsored by BookLocal. */}
-                        Welcome to the book local booking app. If you've received an email about a free stay at ethmemphis click here, to learn more about us click here.
-                    </h2>
-                </div>
-            </div>
-        )
-    }
-}
+const RegisterMessage = () => (
+
+    <div className="home">
+        <div className="register-message">
+            <h2>
+                {/* Register for a free stay at the Exchange Building during ETHMemphis this May. <br/>Sponsored by BookLocal. */}
+                Welcome to the book local booking app. If you've received an email about a free stay at ethmemphis click <Link to="/register">here</Link>, to learn more about us click <Link to="/about">here</Link>.
+            </h2>
+        </div>
+    </div>
+
+)
+
+export default RegisterMessage
