0e53d133f1115a6aa514ab48a371d29fce88e9b9
SamSteele01 <ssteele017@gmail.com>
2018-04-09 00:18:58 -0400

    Change background image

--- src/containers/Status.jsx
+++ src/containers/Status.jsx
@@ -22,7 +22,7 @@ export default class Status extends Component {
 
   render() {
     return (
-        <div className="app">
+        <div className="home darken margin-at-bottom">
             <GetNextReservation web3={this.props.web3} RR={this.props.RR} tokenId={this.state.tokenId} returnComponentState={this.returnComponentState} checkInDate={this.props.checkInDate} checkOutDate={this.props.checkOutDate} />
             {this.state.tokenId &&
                 <div className="z-above-after">
