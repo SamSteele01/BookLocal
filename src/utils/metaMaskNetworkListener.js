1eac357bb213bd8bedea2cead576b9065e5c7f20
SamSteele01 <ssteele017@gmail.com>
2018-04-10 00:42:59 -0400

    Uncomment code

--- src/utils/metaMaskNetworkListener.js
+++ src/utils/metaMaskNetworkListener.js
@@ -1,32 +1,40 @@
+
+
 // var account = web3.eth.accounts[0];
 // var accountInterval = setInterval(function() {
 //   if (web3.eth.accounts[0] !== account) {
 //     account = web3.eth.accounts[0];
 //     updateInterface();
 //   }
-// }, 100);
+// }, 1000);
 
-// web3.version.getNetwork((err, netId) => {
-//   switch (netId) {
-//     case "1":
-//       console.log('This is mainnet')
-//       break
-//     case "2":
-//       console.log('This is the deprecated Morden test network.')
-//       break
-//     case "3":
-//       console.log('This is the ropsten test network.')
-//       break
-//     case "4":
-//       console.log('This is the Rinkeby test network.')
-//       break
-//     case "42":
-//       console.log('This is the Kovan test network.')
-//       break
-//     default:
-//       console.log('This is an unknown network.')
-//   }
-// })
+
+const whichNetwork = this.state.web3.version.getNetwork((err, netId) => {
+  switch (netId) {
+    case "1":
+      console.log('This is mainnet')
+      break
+    case "2":
+      console.log('This is the deprecated Morden test network.')
+      break
+    case "3":
+      console.log('This is the ropsten test network.')
+      break
+    case "4":
+      console.log('This is the Rinkeby test network.')
+      break
+    case "42":
+      console.log('This is the Kovan test network.')
+      break
+    default:
+      console.log('This is an unknown network.')
+  }
+  if(err){
+    console.log('err: ', err);
+  }
+})
+
+export default whichNetwork
 
 // var transaction = web3.eth.getTransaction('0x9fc76417374aa880d4449a1f7f31ec597f00b1f6f3dd2d66f4c9c6c445836d8b', function(error, result){
 //     if(!error)
