5e0fa06120281f8931c6a52c303241bb0de3222a
SamSteele01 <ssteele017@gmail.com>
2018-04-12 18:03:46 -0400

    Add MIT and Ethereum symbols. Add classnames

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import EthereumSymbol from "images/Ethereum_logo.png";
 
 export default class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                <footer >
-                    Copyright © 2018 BookLocal, All rights reserved
-                </footer>
+                {/* <footer > */}
+                <div className="flex-row-center mit-symbol">
+                    <i class="fas fa-balance-scale"></i> MIT License 
+                </div>
+                <div className="flex-row-center ethereum-symbol">
+                    <img className="image-xsml" alt="Ethereum symbol" src={EthereumSymbol}/>  Powered by Ethereum
+                </div>
+                {/* </footer> */}
             </div>
         )
     }
