1f5f94f42bb65ff433f6c0f66a95317402481a35
SamSteele01 <ssteele017@gmail.com>
2018-04-25 15:44:38 -0400

    Use IronImage component

--- src/components/RegisterMessage.jsx
+++ src/components/RegisterMessage.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import IronImage from "components/IronImage"
+import SmallBackground from "images/booklocal-bg-sm.jpg"
+import BackgroundImage from "images/booklocal-bg.jpg"
 
 const RegisterMessage = () => (
 
-    <div className="home">
-        <div className="register-message dark-glass margin">
-            <h2>
+    <div 
+    className="home"
+    >
+        <IronImage srcPreload={SmallBackground} srcLoaded={BackgroundImage} />
+        {/* <div className="register-message dark-glass margin">
+            <h2> */}
                 {/* Register for a free stay at the Exchange Building during ETHMemphis this May. <br/>Sponsored by BookLocal. */}
-                Welcome to BookLocal. 
-            </h2>
+                {/* Welcome to BookLocal.  */}
+            {/* </h2>
             <p>If you've received an email about a free stay at EthMemphis, click <Link to="/register">here</Link>.</p>
             <p> To learn more about us, click <Link to="/about">here</Link>.</p>
-        </div>
+        </div> */}
     </div>
 
 )
