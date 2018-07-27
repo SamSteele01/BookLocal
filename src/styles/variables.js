cc53e71a3efbfed158edb1dc0e7c22882ae63f4e
Sean <ninceur@gmail.com>
2018-07-27 16:01:12 -0400

    Moved all .scss files into the partials folder and renamed to .css. Added imports for these into App.css, except for variables. Variables were moved into variables.js. No style warnings as of this commit.

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -9,6 +9,40 @@ var globalVariable = {
   fontColorLight: '#fff',
   fontColorDark: 'black',
   fontColorHeader: '#05406E',
-  fontSize: '1rem'
+  fontSize: '1rem',
+  // variables from v2 SASS variables.scss
+  /*Colors*/
+  orange1: 'rgb(231, 158, 109)',
+  orange2: 'rgb(226, 124, 57)',
+  orange3: '#E66E1C',
+  white: '#FFFFFF',
+  // logo-blue: #3C5BBE',
+  logoBlue: '#1b75bb',
+  blue1: 'rgb(128, 178, 231)',
+  blue2: '#185FAC',
+  blue3: 'rgb(13, 71, 133)',
+  fontGray: '#464749',
+  headerGray: '#3C3C3C',
+  gray: 'rgb(71, 71, 71)',
+  navyBlue: '#0c1a2b',
+  parchment: 'rgb(236, 236, 236)',
+
+  //Break points
+  smMobile: '370px',
+  mobile: '425px',
+  tablet: '768px',
+  desk: '992px',
+  xldesk: '1200px',
+
+  //Heights
+  headerHeight: '86px',
+  headerTabletHeight: '102px',
+  footerHeight: '45px',
+  // typography
+  mainFont: "'Open Sans', sans-serif",
+  headerFont: "'Titillium Web', sans-serif",
+  bodyFont: "'Oswald', 'Arial Narrow', sans-serif",
+  secondaryHeader: "'PT Sans Narrow', serif"
+
 }
 module.exports = globalVariable
