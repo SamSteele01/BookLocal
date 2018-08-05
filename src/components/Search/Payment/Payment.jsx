e81ceef92568fbe4b8b3ad63758b140e8ae0ee62
SamSteele01 <ssteele017@gmail.com>
2018-08-04 21:22:47 -0400

    import lokka and create mutate and contract.reserve functions

--- src/components/Search/Payment/Payment.jsx
+++ src/components/Search/Payment/Payment.jsx
@@ -1,24 +1,35 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import getWeb3 from 'utils/getWeb3'
-import TextInput from 'components/TextInput'
+import { Lokka } from 'lokka'
+import { Transport } from 'lokka-transport-http'
+// import getWeb3 from 'utils/getWeb3'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import moment from 'moment'
+
+import TextInput from 'components/TextInput'
+import ResDetails from '../ResDetails';
 import './Payment.css'
 
+const client = new Lokka({
+  transport: new Transport('http://localhost:8080/graphql')
+});
 
 class Payment extends Component {
   static propTypes = {
     web3: PropTypes.object.isRequired,
-    contract: PropTypes.object,
-    address: PropTypes.string
+    hotelContract: PropTypes.object,
+    roomTypeIndex: PropTypes.number,
+    address: PropTypes.string,
+    showPaymentForm: PropTypes.func,
+    reservationData: PropTypes.object,
+    bookingDates: PropTypes.array,
   }
   constructor(props) {
     super(props)
     this.state = {
       paymentAmountETH: null,
-      walletAddress: null,
+      walletAddress: window.web3.eth.accounts[0],
       submitted: false,
       paymentTypeToggle: false,
       transactionId: null,
@@ -28,11 +39,7 @@ class Payment extends Component {
   this.handleSubmit=this.handleSubmit.bind(this);
   this.handleTextChange=this.handleTextChange.bind(this);
   }
-  
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("Payment submission sent!");
-  }
+
   handleTextChange = (event) => {
     if(this.state[event.target.id] !== undefined) {
       this.setState({[event.target.id]: event.target.value});
@@ -40,6 +47,58 @@ class Payment extends Component {
   }
   hidePaymentForm = (event) => {
     this.props.showPaymentForm(false);
+  }
+  dateConverter = (mmddyyyy) => {
+    return Math.floor(moment(mmddyyyy).unix() / 86400);
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let web3 = this.props.web3;
+    this.props.hotelContract.reserve(
+      this.props.roomTypeIndex,
+      this.dateConverter(this.props.checkInDate),
+      this.dateConverter(this.props.checkOutDate),
+      {
+        from: web3.eth.accounts[0],
+        value: web3.toWei(this.props.price, 'ether'),
+        gas: 3000000
+      },
+      (err,res) => {
+        if (err) {
+          console.log('availability: "false" '+err);
+          this.setState({ availability: `Oops! Something went wrong ${err}` })
+        } else {
+          this.setState({
+            response: res, // switch to response page
+            account: web3.eth.accounts[0],
+          })
+          this.setTxnListener(res)
+        }
+      }
+    );
+  }
+
+  async submitTravelerInfo(input) {
+    // hotelAddressArray => hotelDataArray
+    let traveler = ''
+    let address = ''
+    let string = `($input: TravelerInput!){
+      addNewTraveler(input: $input){
+        id
+        ethAddress
+        name
+        phoneNumber
+        preferences
+      }
+    }`
+    let variable = { input: input }
+    await client.mutate(string, variable)
+    .then(data => {
+      traveler = data
+      address = data.addNewTraveler.ethAddress
+    }).catch(error => console.log(error))
+    return { traveler, address }
   }
 
   render() {
