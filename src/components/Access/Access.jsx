022bd7bf0ee53aebee1c9670ab37883c9faea744
SamSteele01 <ssteele017@gmail.com>
2018-08-04 21:10:19 -0400

    modify to display traveler and reservation data

--- src/components/Access/Access.jsx
+++ src/components/Access/Access.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { PulseLoader } from 'react-spinners';
+import './access.css';
 
 let access;
 
 class Access extends Component{
+  static propTypes = {
+
+  }
   constructor(props){
     super(props)
     this.state = {
       tokenId : this.props.tokenId,
-      response: null,
-      blockNum: null,
-      status: null  // '0x0' = fail '0x1' = success
+      displayAccessCard: false,
+      traveler: {},
+      hotel: {},
+      hotelProfile: {},
+      renderDetails: false,
     }
 
     this.handleSubmit=this.handleSubmit.bind(this);
@@ -26,21 +32,30 @@ class Access extends Component{
   handleSubmit = (event) => {
     event.preventDefault();
     console.log("Access fired!");
-    access = this.props.RR.access(this.state.tokenId,
-      {from: this.props.web3.eth.accounts[0], gas: 3000000},
-      (err,res) => {
-        if(err){
-          console.log(
-            'Error getting next reservation '+err
-          );
-        }
-        console.log(res);
-        this.setState({
-          response: res
-        });
-        this.setTxnListener(res)
+    // query server with reservation address or traveler Id
+
+    // get data on hotel, reservation, roomType
+    // redirect
+    this.setState({
+      displayAccessCard: true,
+      traveler: { name: 'Sam Steele' },
+      hotelProfile: {
+        name: 'The Exchange Building',
+        city: 'Memphis',
+        state: 'TN'
+      },
+      reservation: {
+        // ethAddress: "0x67dead453beef09034",
+        initialBalence: 2.2,
+        price: 2.2,
+        checkIn: 17747,
+        checkOut: 17749,
       }
-    );
+    })
+  }
+
+  toggleDetailsRender = () => {
+    this.setState({ renderDetails: !this.state.renderDetails })
   }
 
   setTxnListener = (txn) => {
@@ -79,47 +94,96 @@ class Access extends Component{
     return(
       <div className="home darken">
         <div className="access">
-        <fieldset>
-            { this.state.response ?
-              <div>
-                { (this.state.blockNum && this.state.status!==null) ?
-                  <div>
-                    { this.state.status==="0x1" ?
-                      <div>
-                        <h1>Checked In!</h1>
-                        <div>Please enjoy your stay!</div>
-                      </div>
-                    :
-                      <div className="reserve-warning">There was a problem with checking in. Please contact Steve Lee: steven@booklocal.in</div>
-                    }
-                  </div>
-                :
-                    // spinner
-                  <div>
-                    <PulseLoader color='#1b75bb' loading={true} />
-                    <div>Please wait while the transaction gets mined. This could take a minute or two.</div>
-                  </div>
-                }
-              </div>
-            :
-              <div>
-                <h1>Check In</h1>
-                <div className="label-style">Token Id:
-                  <input id="tokenId" type="text" className="input-style" onChange={this.handleTextChange} value={this.state.tokenId} />
+          { !this.state.displayAccessCard ?
+            <fieldset>
+              { this.state.response ?
+                <div>
+                  { (this.state.blockNum && this.state.status!==null) ?
+                    <div>
+                      { this.state.status==="0x1" ?
+                        <div>
+                          <h1>Checked In!</h1>
+                          <div>Please enjoy your stay!</div>
+                        </div>
+                      :
+                        <div className="reserve-warning">There was a problem with checking in. Please contact Steve Lee: steven@booklocal.in</div>
+                      }
+                    </div>
+                  :
+                      // spinner
+                    <div>
+                      <PulseLoader color='#1b75bb' loading={true} />
+                      <div>Please wait while the transaction gets mined. This could take a minute or two.</div>
+                    </div>
+                  }
                 </div>
-                {this.props.account===null || this.props.account===undefined ?
-                  <div>
-                    <div className="reserve-warning">Please log in to MetaMask.</div>
-                    <input id="search" type="submit" className="input-button-style disabled" value="Check In" onClick={this.handleSubmit} disabled/>
+              :
+                <div>
+                  <h1>Check In</h1>
+                  <div>Make sure to set MetaMask to the same address that was used to reserve your room.</div>
+                  <div className="label-style">Address:
+                    <input id="tokenId" type="text" className="input-style" onChange={this.handleTextChange} value={this.state.tokenId} />
                   </div>
-                :
-                  <input id="search" type="submit" className="input-button-style" value="Check In" onClick={this.handleSubmit} />
-                }
-                {/* <input id="search" type="submit" value="Check In" className="input-button-style" onClick={this.handleSubmit} /> */}
-
+                  {this.props.account===null || this.props.account===undefined ?
+                    <div>
+                      <div className="reserve-warning">Please log in to MetaMask.</div>
+                      <input id="search" type="submit" className="input-button-style disabled" value="Check In" onClick={this.handleSubmit} disabled/>
+                    </div>
+                  :
+                    <input id="search" type="submit" className="input-button-style" value="Check In" onClick={this.handleSubmit} />
+                  }
+                </div>
+              }
+            </fieldset>
+          :
+            <fieldset>
+              <div className="label-style">
+                Hello {this.state.traveler.name}
               </div>
-            }
-          </fieldset>
+              <div className="label-style">
+                Upcoming stays:
+              </div>
+              <div styleName="wire-frame-box">
+                <div styleName="row space-around" >
+                  <div className="label-style" styleName="h-spacing">
+                    {this.state.hotelProfile.name}
+                  </div>
+                  <div className="label-style" styleName="h-spacing">
+                    {this.state.hotelProfile.city}, {this.state.hotelProfile.state}
+                  </div>
+                </div>
+                <div styleName="row space-around" >
+                  <div className="label-style" styleName="h-spacing">
+                    {this.state.reservation.checkIn}
+                  </div>
+                  <div className="label-style" styleName="h-spacing">
+                    {this.state.reservation.checkOut}
+                  </div>
+                </div>
+                { this.state.renderDetails &&
+                  <div>
+
+                    <div className="label-style">
+                      Info:
+                    </div>
+
+                    <div className="label-style">
+                      Directions:
+                    </div>
+                    <div className="label-style">
+                      Hotel Amenities:
+                    </div>
+                    <div className="label-style">
+                      Wallet:
+                    </div>
+                  </div>
+                }
+                <div className="input-button-style" onClick={this.toggleDetailsRender}>
+                  { this.state.renderDetails ? "Hide Details" : "Click to Access" }
+                </div>
+              </div>
+            </fieldset>
+          }
         </div>
       </div>
     )
