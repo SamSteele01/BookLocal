86ff84e8f94ddcf32245c097437772bad5f7284e
SamSteele01 <ssteele017@gmail.com>
2018-08-09 23:34:30 -0400

    change message/text

--- src/components/RegisterMessage.jsx
+++ src/components/RegisterMessage.jsx
@@ -10,14 +10,14 @@ const RegisterMessage = () => (
     className="home"
     >
         <IronImage srcPreload={SmallBackground} srcLoaded={BackgroundImage} />
-        {/* <div className="register-message dark-glass margin">
-            <h2> */}
-                {/* Register for a free stay at the Exchange Building during ETHMemphis this May. <br/>Sponsored by BookLocal. */}
-                {/* Welcome to BookLocal.  */}
-            {/* </h2>
-            <p>If you've received an email about a free stay at EthMemphis, click <Link to="/register">here</Link>.</p>
-            <p> To learn more about us, click <Link to="/about">here</Link>.</p>
-        </div> */}
+        <div className="register-message dark-glass margin">
+            <h2>
+                Welcome to BookLocal.
+            </h2>
+            <h2>
+                The hotel booking app powered by Ethereum.
+            </h2>
+        </div>
     </div>
 
 )
