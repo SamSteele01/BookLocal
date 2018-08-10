e52f659ef1cd122a36846e851ec1f805c6677691
SamSteele01 <ssteele017@gmail.com>
2018-08-09 22:42:10 -0400

    get web3 from window

--- src/components/HotelContractWrapper/HotelContractWrapper.jsx
+++ src/components/HotelContractWrapper/HotelContractWrapper.jsx
@@ -6,16 +6,15 @@ import Payment from 'components/Search/Payment/Payment'
 export default class HotelContractWrapper extends Component {
 
   static propTypes = {
-    web3: PropTypes.object.isRequired,
+    // web3: PropTypes.object.isRequired,
     address: PropTypes.string.isRequired, // hotelAddress
-    // view: PropTypes.string // pass through
   };
 
   constructor(props) {
     super(props);
 
     this.state = {
-      contract: props.web3.eth.contract(hotelABI).at(this.props.address),
+      contract: window.web3.eth.contract(hotelABI).at(this.props.address),
     }
   }
 
@@ -24,9 +23,6 @@ export default class HotelContractWrapper extends Component {
     return (
       <div>
         <Payment
-          // web3={this.props.web3}
-          // address={this.state.hotelAddress}
-          // view={this.props.view}
           hotelContract={this.state.contract}
           {...passThroughProps}
         />
