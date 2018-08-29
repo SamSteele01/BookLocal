4160b2606db0a013114e8b23b58c0c466f18b4d0
SamSteele01 <ssteele017@gmail.com>
2018-08-29 03:37:28 -0400

    move search related code to seperate component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css?raw'
-import SearchForm from './SearchForm'
-import SearchResults from './SearchResults'
+import SearchView from './SearchView'
 import TravelerInfoForm from './TravelerInfoForm'
 import HotelContractWrapper from 'components/HotelContractWrapper';
 import uniqueId from 'react-html-id'
-import Payment from './Payment'
 
 import './Search.css'
 
@@ -18,16 +13,9 @@ class Search extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      // search input
-        searchString: '',
-        startDate: moment(),
-        endDate: moment().add(1, 'd'),
-      // search
-        searchQuery: {},
-        searchResults: [],
       // nav
         searchSubmitted: false,
-        showReservationForm: false,
+        showTravelerInfoForm: false,
         showPaymentForm: false,
       // to payment
         hotelAddress: null, // hard code, eventually from Results
@@ -42,62 +30,27 @@ class Search extends Component {
     // this.handleTextChange=this.handleTextChange.bind(this);
   }
 
-  handleTextChange = (event) => {
-    if(this.state[event.target.id] !== undefined) {
-      this.setState({[event.target.id]: event.target.value});
-    }
-  }
-  setSearchString = (string) => {
-    this.setState({ searchString: string });
-  }
-  handleDateChangeStart = (date) => {
-      this.setState({ startDate: date });
-    }
-  handleDateChangeEnd = (date) => {
-    this.setState({ endDate: date });
-    }
-  dateConverter = (mmddyyyy) => {
-    return Math.floor(moment(mmddyyyy).unix() / 86400);
-    }
-
-  handleSubmitHotelSearch = (event) => {
-    event.preventDefault();
-    console.log("Search fired!");
-    // convert time strings for use by DB
-    let query = {
-      searchString: this.state.searchString,
-      start: this.dateConverter(this.state.startDate),
-      end: this.dateConverter(this.state.endDate),
-    }
+  returnSearchResults = (query) => {
+    // send data to SearchResults
     this.setState({
-      searchQuery: query,
-      searchSubmitted: true,
-    })
-    // bringing in this.state.searchString, start, and end
-    // this.returnSearchResults(query);
+      showTravelerInfoForm: true,
+    });
   }
-
-  // returnSearchResults = (query) => {
-  //   // send data to SearchResults
-  //   this.setState({
-  //     showReservationForm: true,
-  //   });
-  // }
 
   /* get from SearchResultsItem, pass to HotelContractWrapper, to Payment */
   reservationData = (resData) => {
     this.setState({
-      reservationData: resData,
+      reservationData: resData, // ? => vvv
       hotelAddress: "0x88982d815bc97c00e42d4e91b5d73d0c6ae01a5b",
       roomTypeIndex: 1,
       price: resData.totalPrice,
-      showReservationForm: true,
+      showTravelerInfoForm: true,
     })
   }
 
   /* Navigation */
   backToSearchResults = (event) => {
-    this.setState({ showReservationForm: false });
+    this.setState({ showTravelerInfoForm: false });
   }
   showPaymentForm = (event) => {
     console.log('spf !')
@@ -114,7 +67,7 @@ class Search extends Component {
         {/* <div styleName="style.container-header">
           <h1>Search for a hotel</h1>
         </div> */}
-        { this.state.showReservationForm ?
+        { this.state.showTravelerInfoForm ?
           <TravelerInfoForm
             showPaymentForm={this.showPaymentForm}
             backToSearchResults={this.backToSearchResults}
@@ -124,55 +77,9 @@ class Search extends Component {
             reservationData={this.state.reservationData}
           />
         :
-          <div styleName="search-container">
-            <div styleName="search-bar">
-              <div className="formGroup" styleName="citySearch">
-                <SearchForm
-                  setSearchString={this.setSearchString}
-                />
-              </div>
-              <div className="formGroup" styleName="startDate">
-                <label htmlFor={this.nextUniqueId()}>Start Date</label>
-                <DatePicker
-                  id={this.lastUniqueId()}
-                  selected={this.state.startDate}
-                  onChange={this.handleDateChangeStart}
-                  selectsStart
-                  // maxDate={this.state.endDate}
-                  startDate={this.state.startDate}
-                  endDate={this.state.endDate}
-                  placeholderText="Check-in date"
-                  className="startDate-datePicker formInput"
-                  htmlId="startDate"
-                />
-              </div>
-              <div className="formGroup" styleName="endDate">
-                <label htmlFor={this.nextUniqueId()}>End Date</label>
-                <DatePicker
-                  id={this.lastUniqueId()}
-                  selected={this.state.endDate}
-                  onChange={this.handleDateChangeEnd}
-                  SelectsEnd
-                  minDate={this.state.startDate}
-                  startDate={this.state.startDate}
-                  endDate={this.state.endDate}
-                  placeholderText="Check-out Date"
-                  className="endDate-datePicker formInput"
-                  htmlId="endDate"
-                />
-              </div>
-              <div className="formGroup" styleName="searchSubmit">
-                <button
-                  onClick={this.handleSubmitHotelSearch}
-                  className="button"
-                  styleName="searchSubmitButton"
-                  value="Search for hotels"
-                >
-                  Search for hotels
-                </button>
-              </div>
-            </div>
-          </div>
+          <SearchView
+            reservationData={this.reservationData}
+          />
         }
         { this.state.showPaymentForm &&
           <div
@@ -194,16 +101,6 @@ class Search extends Component {
               price={this.state.price}
               reservationData={this.state.reservationData}
               showPaymentForm={this.showPaymentForm}
-            />
-          </div>
-        }
-        { this.state.searchSubmitted &&
-          <div styleName="results">
-            <SearchResults
-              searchQuery={this.state.searchQuery}
-              searchRunning={this.state.searchSubmitted}
-              reserveClicked={this.reserveClicked}
-              reservationData={this.reservationData}
             />
           </div>
         }
---
+++ src/components/Search/SearchView/SearchView.jsx
@@ -0,0 +1,200 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css?raw'
+import { Lokka } from 'lokka'
+import { Transport } from 'lokka-transport-http'
+import SearchForm from './SearchForm'
+import SearchResults from './SearchResults'
+import hotelABI from 'abis/hotelABI'
+
+const client = new Lokka({
+  transport: new Transport('http://localhost:8080/graphql')
+});
+
+export default class SearchView extends Component {
+
+  static propTypes = {
+    reservationData: PropTypes.func.isRequired
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // search input
+      city: '',
+      startDate: moment(),
+      endDate: moment().add(1, 'd'),
+      // search results
+      display: null,
+      hotelResultsArray: [],
+    };
+
+  }
+
+  // handleTextChange = (event) => {
+  //   if(this.state[event.target.id] !== undefined) {
+  //     this.setState({[event.target.id]: event.target.value});
+  //   }
+  // }
+  setCity = (string) => {
+    this.setState({ city: string });
+  }
+  handleDateChangeStart = (date) => {
+    this.setState({ startDate: date });
+  }
+  handleDateChangeEnd = (date) => {
+    this.setState({ endDate: date });
+  }
+  dateConverter = (date) => {
+    return Math.floor(moment(date).unix() / 86400);
+  }
+
+  /* (city, checkIn, checkOut) =>
+  * query allHotels(city) { return hotel, profile, [roomTypes] }
+  * listOfHotels.map => { compile [roomTypes] }
+  * listOfRoomTypes.filter(hasAvailability(roomType, checkIn, checkOut)) =>
+  * "goList".map => search results
+  *
+  * data = {
+  * hotelName
+  * hotelImage
+  * hotelAddress *
+  * roomTypeIndex *
+  * beds
+  * price *
+  * }
+  */
+
+  async getHotelDataFromDB(city) {
+    // hotelAddressArray => hotelDataArray
+    let hotelsArray = ''
+    await client.query(`{
+      allHotels(city: ${city}){
+        id
+        ethAddress
+        hotelProfile{
+          id
+          name
+          city
+          images
+        }
+        roomTypes{
+          id
+          index
+          beds
+          price
+        }
+      }
+    }`)
+    .then(data => {
+      if (data.hotel !== null) {
+        hotelsArray = data.allHotels
+      }
+    }).catch(error => {
+      console.log(error)
+      this.setState({ queryError: 'query error' })
+    })
+    return hotelsArray
+  }
+
+  /* assumes that web3 is provided by the browser */
+
+  async handleSubmitHotelSearch(event) {
+    event.preventDefault();
+    console.log("Search fired!");
+    this.setState({ display: 'loading' })
+    /* getHotelDataFromDB(city) => hotelsArray */
+    let hotelsArray = await this.getHotelDataFromDB(this.state.city)
+    /* filter rooms with no availability */
+    let hotelsWithAvailableRooms = await hotelsArray.map(hotel => {
+      let availableRooms = hotel.roomTypes.filter(async roomType => {
+        await window.web3.eth.contract(hotelABI).at(hotel.ethAddress)
+          .hasAvailability(
+            roomType.index,
+            this.dateConverter(this.state.startDate),
+            this.dateConverter(this.state.endDate),
+          )
+      })
+      return availableRooms.map(room => {
+        return Object.assign(hotel, { roomTypes: room })
+      })
+    })
+    console.log('HOTELSWITHAVAILABLEROOMS', hotelsWithAvailableRooms)
+    /* filter hotels without rooms */
+    // let hotelResultsArray = hotelsWithAvailableRooms.filter(hotel =>
+    //   hotel.roomTypes.length >= 1
+    // )
+    /* return */
+    this.setState({
+      hotelResultsArray: hotelsWithAvailableRooms,
+      display: 'results'
+    })
+  }
+
+  render() {
+    return (
+      <div styleName="search-container">
+        <div styleName="search-bar">
+          <div className="formGroup" styleName="citySearch">
+            <SearchForm
+              setCity={this.setCity}
+            />
+          </div>
+          <div className="formGroup" styleName="startDate">
+            <label htmlFor={this.nextUniqueId()}>Start Date</label>
+            <DatePicker
+              id={this.lastUniqueId()}
+              selected={this.state.startDate}
+              onChange={this.handleDateChangeStart}
+              selectsStart
+              // maxDate={this.state.endDate}
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              placeholderText="Check-in date"
+              className="startDate-datePicker formInput"
+              htmlId="startDate"
+            />
+          </div>
+          <div className="formGroup" styleName="endDate">
+            <label htmlFor={this.nextUniqueId()}>End Date</label>
+            <DatePicker
+              id={this.lastUniqueId()}
+              selected={this.state.endDate}
+              onChange={this.handleDateChangeEnd}
+              SelectsEnd
+              minDate={this.state.startDate}
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              placeholderText="Check-out Date"
+              className="endDate-datePicker formInput"
+              htmlId="endDate"
+            />
+          </div>
+          <div className="formGroup" styleName="searchSubmit">
+            <button
+              onClick={this.handleSubmitHotelSearch}
+              className="button"
+              styleName="searchSubmitButton"
+              value="Search for hotels"
+            >
+              Search for hotels
+            </button>
+          </div>
+        </div>
+        { this.state.display &&
+          <div styleName="results">
+            <SearchResults
+              display={this.state.display}
+              hotelResultsArray={this.state.hotelResultsArray}
+              reservationData={this.props.reservationData}
+            />
+          </div>
+        }
+      </div>
+    );
+  }
+
+}
